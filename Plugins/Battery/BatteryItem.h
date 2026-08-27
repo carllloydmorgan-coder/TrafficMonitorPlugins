0f2feac828c901b894c79db6f3beff87a4f52184
carllloydmorgan-coder <carl.lloyd.morgan@gmail.com>
2026-08-26 22:15:50 -0500

    Add files via upload

--- Plugins/Battery/BatteryItem.h
+++ Plugins/Battery/BatteryItem.h
@@ -15,4 +15,6 @@ public:
     virtual bool IsCustomDraw() const override;
     virtual int GetItemWidthEx(void* hDC) const override;
     virtual void DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode) override;
+    //在任务栏中始终独占双行（占满整个窗口高度），其他显示项目会两个一组排列在旁边
+    virtual int IsDoubleLineExclusive() const override { return 1; }
 };
